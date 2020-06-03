b54f8a8b2a3158ddf4653234c41816ab6e509797
HIEU VU THANH <jezzs2k@gmail.com>
2020-06-03 17:39:52 +0700

    menu feature

--- src/components/layers/Menu.js
+++ src/components/layers/Menu.js
@@ -1,9 +1,21 @@
 import React, { Fragment } from 'react';
 
 import './menu.css';
+import MenuIcon from '../image/menu.svg';
 
 const Menu = () => {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className='menu'>
+        <div className='btn'>
+          <img src={MenuIcon} className='btn-icon' alt='Menu-icon' />
+        </div>
+        <div className='logo'>
+          <h2 className='name'>DAILIST</h2>
+        </div>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Menu;
