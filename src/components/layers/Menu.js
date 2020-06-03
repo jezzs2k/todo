222f8ab5f5a85670294d19cf6b7e02483498efca
HIEU VU THANH <jezzs2k@gmail.com>
2020-06-03 17:44:34 +0700

    delete fragment and inline style in index.html

--- src/components/layers/Menu.js
+++ src/components/layers/Menu.js
@@ -1,20 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import './menu.css';
 import MenuIcon from '../image/menu.svg';
 
 const Menu = () => {
   return (
-    <Fragment>
-      <div className='menu'>
-        <div className='btn'>
-          <img src={MenuIcon} className='btn-icon' alt='Menu-icon' />
-        </div>
-        <div className='logo'>
-          <h2 className='name'>DAILIST</h2>
-        </div>
+    <div className='menu'>
+      <div className='btn'>
+        <img src={MenuIcon} className='btn-icon' alt='Menu-icon' />
       </div>
-    </Fragment>
+      <div className='logo'>
+        <h2 className='name'>DAILIST</h2>
+      </div>
+    </div>
   );
 };
 
