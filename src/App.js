d9f80b154e42a782587237f4cb69b51d219a162f
HIEU VU THANH <jezzs2k@gmail.com>
2020-06-03 18:06:56 +0700

    add Home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Home from './components/pages/Home';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,25 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import './home.css';
+import Menu from '../layers/Menu';
 
 const Home = () => {
-  return <Fragment></Fragment>;
+  return (
+    <div className='content'>
+      <div className='header'>
+        <Menu />
+      </div>
+      <div className='bg'>
+        <div className='text'>
+          <p>Seems like</p>
+          <p>You have no list</p>
+        </div>
+      </div>
+      <div className='footer'>
+        <p className='text'>@copyright by jezzs</p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
