b2aeeb9412d57ce8638674247e2bb4a1f44fe623
HIEU VU THANH <jezzs2k@gmail.com>
2020-06-03 20:28:40 +0700

    create todo and finish todos

--- src/components/toDoList/ListToDo.js
+++ src/components/toDoList/ListToDo.js
@@ -1,9 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropsTypes from 'prop-types';
 
 import './listToDo.css';
+import TaskItem from './TaskItem';
+
+const listTask = [
+  { number: 1, text: 'make have dinner', isComplete: false },
+  {
+    number: 2,
+    text: 'make have breakfast',
+    isComplete: false,
+  },
+  { number: 3, text: 'make have lunch', isComplete: false },
+];
 
 const ListToDo = () => {
-  return <Fragment></Fragment>;
+  return (
+    <div className='listToDo'>
+      <h2 className='title'>UPCOMING</h2>
+      {listTask &&
+        listTask.map((task) => {
+          return <TaskItem task={task} />;
+        })}
+    </div>
+  );
+};
+
+ListToDo.propsTypes = {
+  listTask: PropsTypes.array.isRequired,
 };
 
 export default ListToDo;
--- src/components/toDoList/TaskFinish.js
+++ src/components/toDoList/TaskFinish.js
@@ -1,9 +1,33 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import PropsTypes from 'prop-types';
 
-import './taskFinish.css';
+import './listToDo.css';
+import TaskFinishItem from './TaskFinishItem';
+
+const taskFinish = [
+  { number: 1, text: 'make have dinner', isComplete: false },
+  {
+    number: 2,
+    text: 'make have breakfast',
+    isComplete: false,
+  },
+  { number: 3, text: 'make have lunch', isComplete: false },
+];
 
 const TaskFinish = () => {
-  return <Fragment></Fragment>;
+  return (
+    <div className='listToDo'>
+      <h2 className='title'>COMPLETE</h2>
+      {taskFinish &&
+        taskFinish.map((task) => {
+          return <TaskFinishItem task={task} />;
+        })}
+    </div>
+  );
+};
+
+TaskFinish.propsTypes = {
+  listTask: PropsTypes.array.isRequired,
 };
 
 export default TaskFinish;
